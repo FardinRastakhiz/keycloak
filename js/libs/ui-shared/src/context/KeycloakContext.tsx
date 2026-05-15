fe87e45e2f6954840e76c640023661ad9588557a
DESKTOP-1UVPMQU\Sepehr <fardin.rastakhiz@gmail.com>
2026-05-15 18:55:51 +0330

    update docker-compose.yaml and KeycloakContext

--- js/libs/ui-shared/src/context/KeycloakContext.tsx
+++ js/libs/ui-shared/src/context/KeycloakContext.tsx
@@ -80,7 +80,6 @@ export const KeycloakProvider = <T extends BaseEnvironment>({
         onLoad: "login-required",
         pkceMethod: "S256",
         responseMode: "query",
-        checkLoginIframe: false,
         scope: environment.scope,
       });
 
