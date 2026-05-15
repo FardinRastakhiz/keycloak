408dfcae4ed9e8c01386758bf75edc915fc404bc
DESKTOP-1UVPMQU\Sepehr <fardin.rastakhiz@gmail.com>
2026-05-15 18:21:15 +0330

    Add 'checkLoginIframe: false' to 'keycloak.init' input config inside 'KeycloakContext.tsx' file

--- js/libs/ui-shared/src/context/KeycloakContext.tsx
+++ js/libs/ui-shared/src/context/KeycloakContext.tsx
@@ -80,6 +80,7 @@ export const KeycloakProvider = <T extends BaseEnvironment>({
         onLoad: "login-required",
         pkceMethod: "S256",
         responseMode: "query",
+        checkLoginIframe: false,
         scope: environment.scope,
       });
 
